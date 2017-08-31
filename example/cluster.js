031493707ad2d30fd08ffb615807c982d5c594a9
Ken Morishita <mokemokechicken@gmail.com>
2017-08-31 11:09:15 +0900

    change for test

--- example/cluster.js
+++ example/cluster.js
@@ -5,15 +5,19 @@ var http = require('http');
 
 var createServer = function () {
     return http.createServer(function(req, res) {
+      var worker = "worker "+(cluster.isMaster ? 'master' : cluster.worker.id);
       res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('Hello World! From worker '
-        + (cluster.isMaster ? 'master' : cluster.worker.id)
+      res.end('Hello World! From ' + worker
+        + " x-forwarded-for=" + req.headers['x-forwarded-for']
         + ' with pid: ' + process.pid + ' \n');
+      var msg = worker + ": x-forwarded-for=" + req.headers['x-forwarded-for'];
+      console.log(msg);
     });
 };
 
 var stickyOptions = {
-  proxy: false //activate layer 4 patching
+  num: 2,
+  proxy: true //activate layer 4 patching
 }
 
 var server = sticky(stickyOptions, createServer).listen(port, function() {
